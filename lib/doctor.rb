f310e41a415cf27950024d99fed761bc769d5a20
Allan Farca <allan.farca@gmail.com>
2020-04-22 18:49:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,15 +14,12 @@ class Doctor
   end 
   
   def new_appointment (date, patient)
-    #Appointment.new(date, self, patient)
     Appointment.new(date, patient, self)
   end 
   
   def appointments
     Appointment.all.select do |appt|
-      #binding.pry
       appt.doctor == self
-      #appt.doctor
     end
   end 
   
